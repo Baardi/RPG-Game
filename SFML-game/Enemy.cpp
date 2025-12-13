d7c86eff02939ff25406b6672a0c53884127b164
Bård Sigurd Møller <git@bsmoller.no>
2025-12-13 02:27:22 +0100

    test

--- SFML-game/Enemy.cpp
+++ SFML-game/Enemy.cpp
@@ -1,7 +1,12 @@
 #include "stdafx.h"
 #include "Enemy.hpp"
 
+#include <print>
+
 void Enemy::applyProperties()
 {
+	const std::string test = "test:{}";
+	std::println(std::runtime_format(test), 1);
+
 	m_stats = Stats::get(*this);
 }
